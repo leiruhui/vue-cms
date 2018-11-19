c8a7dd06f761d9eaf25857c593add37b9d9ababc
yu <1075084296@qq.com>
2018-11-19 20:32:57 +0800

    第一次

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// 入口文件
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+// 引入header
+import { Header,Tabbar, TabItem  } from 'mint-ui';
+// 注册header组件
+Vue.component(Header.name, Header);
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
+import './lib/mui/css/mui.min.css'
+
+/* eslint-disable no-new */
+
+new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
+  })
